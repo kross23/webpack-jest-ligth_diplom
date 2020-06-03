de03f6be7748e1601e05ed4f4fdd6b31fa33f0f7
kross <662242@mail.ru>
2020-06-04 00:02:29 +0500

    default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 'use strict';
 
+import 'mdn-polyfills/Node.prototype.append';
+import "@babel/polyfill";
+import 'nodelist-foreach-polyfill';
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+
 import consolelog from './modules/consolelog';
 import countTimer from './modules/countTimer';
+import elementClosest from 'element-closest';
+elementClosest(window);
 consolelog();
 countTimer('14 jun 2020');
